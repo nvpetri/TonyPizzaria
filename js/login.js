fad725f1084bae12e8063a4884ccebaf8dece33c
nvpetri <nicolaspetri18@gmail.com>
2023-12-14 10:03:34 -0300

    js teste login

---
+++ js/login.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const button = document.getElementById('loginn')
+
+function teste() {
+    let email = document.getElementById('email').value
+    let senha = document.getElementById('password').value
+
+    let validaEmail = "admin"
+    let validaSenha = "admin"
+
+    if (email && senha) {
+        if (email == validaEmail && senha == validaSenha) {
+            window.location.href = '../html/pgHome.html'
+        } else {
+            alert('Email e/ou senha incorreto (s)!')
+        }
+    }
+}
+button.addEventListener('click', teste)
